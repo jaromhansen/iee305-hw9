7975bb163e84df383e42921de32323117555c213
jaromhansen <jkhanse7@asu.edu>
2025-12-04 12:26:17 -0700

    Actually add course information endpoint

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ def read_root():
 @app.get("/student/{name}")
 def get_student(name: str):
     return {"message": f"Hello {name}!"}
+
+@app.get("/course")
+def get_course_info():
+    return {"course": "IEE 305",
+            "title": "Information Systems Engineering",
+            "semester": "Fall 2025"}
